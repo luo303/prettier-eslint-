4a68a2bcf3218bafad81ce48feb5b7e06fc27292
xhh <jiatangxi@qq.com>
2025-08-29 10:53:28 +0800

    '继续试错'

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
   <h1>You did it!</h1>
   <p>
     Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
